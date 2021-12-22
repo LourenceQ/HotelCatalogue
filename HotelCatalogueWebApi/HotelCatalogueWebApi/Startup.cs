78c13ee6b559d40e03cdcb1a5ba0c33953752231
LourenceQ <lqfmecatronica@gmail.com>
2021-12-22 15:11:55 -0300

    Criaçao de classes entidade

--- HotelCatalogueWebApi/HotelCatalogueWebApi/Startup.cs
+++ HotelCatalogueWebApi/HotelCatalogueWebApi/Startup.cs
@@ -1,7 +1,9 @@
+using HotelCatalogueWebApi.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -27,7 +29,10 @@ namespace HotelCatalogueWebApi
         public void ConfigureServices(IServiceCollection services)
         {
 
-            services.AddControllers();
+            services.AddDbContext<DatabaseContext>(options => 
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("sqlConnection"));
+            });
 
             services.AddCors(c =>
             {
@@ -41,6 +46,8 @@ namespace HotelCatalogueWebApi
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "HotelCatalogueWebApi", Version = "v1" });
             });
+
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
