6d87a2b4d72b25bcc1f94a47f332c04b4c4116f2
LourenceQ <lqfmecatronica@gmail.com>
2021-12-23 17:20:46 -0300

    Ciraçao e implementação de UnitOfWork

---
+++ HotelCatalogue/IRepository/IUnityOfWork.cs
@@ -0,0 +1,15 @@
+﻿using HotelCatalogue.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelCatalogue.IRepository
+{
+    public interface IUnityOfWork : IDisposable
+    {
+        IGenericRepository<Country> Countries { get; }
+        IGenericRepository<Hotel> Hotels{ get; }
+        Task Save();
+    }
+}
