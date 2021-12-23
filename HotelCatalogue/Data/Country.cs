3765bd47c46ba7094e4985f648be4ba1480d0de9
LourenceQ <lqfmecatronica@gmail.com>
2021-12-23 17:44:40 -0300

    Impelmentação de DTOs

--- HotelCatalogue/Data/Country.cs
+++ HotelCatalogue/Data/Country.cs
@@ -10,5 +10,6 @@ namespace HotelCatalogue.Data
         public int Id { get; set; }
         public string Name { get; set; }
         public string ShortName { get; set; }
+        public virtual IList<Hotel> Hotel { get; set; }
     }
 }
