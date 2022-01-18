a7d1ccdec2b46b3157d02f7e171c39ed4c6d53dc
LourenceQ <lqfmecatronica@gmail.com>
2022-01-18 16:34:25 -0300

    Implementação de controllers com método put

--- HotelCatalogue/Controllers/CountryController.cs
+++ HotelCatalogue/Controllers/CountryController.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using HotelCatalogue.Data;
 using HotelCatalogue.IRepository;
 using HotelCatalogue.Models;
 using Microsoft.AspNetCore.Authorization;
@@ -47,7 +48,7 @@ namespace HotelCatalogue.Controllers
         }
 
         [Authorize]
-        [HttpGet("{id:int}")]
+        [HttpGet("{id:int}", Name = "GetCountry")]
         public async Task<IActionResult> GetByIdCountry(int id)
         {
             try
@@ -63,5 +64,60 @@ namespace HotelCatalogue.Controllers
             }
         }
 
+        [Authorize(Roles = "Administrator")]
+        [HttpPost]
+        public async Task<IActionResult> CreateCountry([FromBody] CreateCountryDTO countryDTO)
+        {
+            if (!ModelState.IsValid)
+            {
+                _logger.LogError($"Invalid POST attempt in {nameof(CreateCountry)}");
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                var country = _mapper.Map<Country>(countryDTO);
+                await _unityOfWork.Countries.Insert(country);
+                await _unityOfWork.Save();
+
+                return CreatedAtRoute("GetCountry", new { id = country.Id }, country);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Something went wrong int the {nameof(CreateCountry)}");
+                return StatusCode(500, "Internal Server Error. Please try again later.");
+            }
+        }
+
+        [Authorize]
+        [HttpPut("{id:int}")]
+        public async Task<IActionResult> UpdateCountry(int id, [FromBody] UpdateCountryDTO countryDTO)
+        {
+            if (!ModelState.IsValid || id < 1)
+            {
+                _logger.LogError($"Invalid POST attempt in {nameof(UpdateCountry)}");
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                var country = await _unityOfWork.Countries.Get(q => q.Id == id);
+                if (country == null)
+                {
+                    _logger.LogError($"Invalid POST attempt in {nameof(UpdateCountry)}"); 
+                    return BadRequest("Submitted data is invalid");
+                }
+                _mapper.Map(countryDTO, country);
+                _unityOfWork.Countries.Update(country);
+                await _unityOfWork.Save();
+
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Something went wrong int the {nameof(UpdateCountry)}");
+                return StatusCode(500, "Internal Server Error. Please try again later.");
+            }
+        }
     }
 }
